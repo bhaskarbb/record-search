dc625be78ba7a17d87e39e5883dbcefd06bc5c6e
Bhaskar <bhaskar@prinseps.com>
2022-01-03 05:51:43 +0530

    Add node server and app files
    The server.ts is the entry point. It connects to database and starts the server.
    app.js defines express app, which handles routes and middlewares.

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { SERVER_CONFIG } from './config';
+
+const { BODY_LIMIT, CORS_ORIGIN, CORS_METHODS } = SERVER_CONFIG;
+const app = express();
+
+// Middleware Initializations
+app.use(pino());
+app.use(cors({ origin: CORS_ORIGIN, methods: CORS_METHODS }));
+app.use(express.json({ limit: BODY_LIMIT }));
+app.use(express.urlencoded({ limit: BODY_LIMIT, extended: true }));
+
+export default app;
