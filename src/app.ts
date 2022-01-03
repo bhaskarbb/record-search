935a88165fdba3954161ae0d31392ee7a1d80c33
Bhaskar <bhaskar@prinseps.com>
2022-01-03 06:08:25 +0530

    Add initalize routes function
    Boilerplate to initialize project routes, add a health check route,
    and handle errors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { SERVER_CONFIG } from './config';
+import { initializeRoutes } from './routes';
 
 const { BODY_LIMIT, CORS_ORIGIN, CORS_METHODS } = SERVER_CONFIG;
 const app = express();
@@ -11,5 +12,8 @@ app.use(pino());
 app.use(cors({ origin: CORS_ORIGIN, methods: CORS_METHODS }));
 app.use(express.json({ limit: BODY_LIMIT }));
 app.use(express.urlencoded({ limit: BODY_LIMIT, extended: true }));
+
+// Initialize Routes
+initializeRoutes(app);
 
 export default app;
