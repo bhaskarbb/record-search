8ea321ae49fb7af678facabca8ed81383e5b10c4
Bhaskar <bhaskar@prinseps.com>
2022-01-03 09:18:45 +0530

    Add unit tests
    Added tests for request validation and services

---
+++ test/mockData.js
@@ -0,0 +1,36 @@
+const mockData = [
+  {
+    key: 'BqOIkrTF',
+    createdAt: new Date('2015-06-03T01:01:52.237Z'),
+    counts: [1401, 1950, 1283],
+    value: 'vHpHAzrmkwtt',
+  },
+  {
+    key: 'AkcKkrLs',
+    value: 'qnfSodsuvVzc',
+    createdAt: new Date('2015-03-08T07:29:46.532Z'),
+    counts: [1964, 1870, 1011],
+  },
+  {
+    key: 'rpIRzoMs',
+    value: 'gSmvTJCnSgrL',
+    createdAt: new Date('2016-04-26T10:58:48.978Z'),
+    counts: [383, 16, 1000],
+  },
+  {
+    key: 'hCXxyuAu',
+    value: 'hfJAfcGGizzJ',
+    createdAt: new Date('2015-11-28T11:47:29.706Z'),
+    counts: [864, 509, 712],
+  },
+  {
+    key: 'JxSsEoVo',
+    value: 'bemcbwhDnLdw',
+    createdAt: new Date('2016-09-08T18:35:41.216Z'),
+    counts: [1574, 1647, 113],
+  },
+];
+
+module.exports = {
+  mockData,
+};
